b99bf067bdf5d46a307e8b3d4a8b89f7e5794487
Kelvyn Gustavo Humanes <gustavohumanes2007@gmail.com>
2024-05-18 18:18:01 -0300

    Sistema de horários!

--- Exercicios/Ex007/JavaScript/Verificador_de_nacionalidade.js
+++ Exercicios/Ex007/JavaScript/Verificador_de_nacionalidade.js
@@ -6,13 +6,14 @@ Verificador.addEventListener('click', Verificar)  //botão para Verificar
 
 function Verificar() { // função para verificar
     var Pais = window.document.getElementById('Nacionalidade').value; // Variável que carrega o valor da nacionalidade
-    var Exibidor = window.document.getElementById('Exibidor2'); // Div na qual vamos colocar o texto dizendo se é brasileiro ou estrangeiro
+    var Exibidor = window.document.getElementById('Exibidor2'); //div para brasileiro ou estrangeiro
 
-    if (Pais.toLowerCase() === 'brasil')// condições para verificar se é brasileiro
+    if (Pais.toLowerCase() === 'brasil') // condições para verificar se é brasileiro
     {
         Exibidor.innerHTML = 'Você é Brasileiro!'; // Implementação do texto
     }
-    else {
-        Exibidor.innerHTML = `Você é ${Pais}`;
+    else
+    {
+        Exibidor.innerHTML = `Você é não é Brasileiro, logo não pode podemos te ajudar.`;
     }
 }
